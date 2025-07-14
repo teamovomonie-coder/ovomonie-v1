57094418d8bdbfcc37b04c5edf3e5b01f2caf5c5
Stephen Bariduura Paago <weseeallnfts@gmail.com>
2025-07-14 20:04:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/community/create-post-dialog.tsx
+++ src/components/community/create-post-dialog.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import Image from 'next/image';
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from '@/components/ui/dialog';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogClose } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
